0ba6c16c33e16494140587e580dd11139118fb09
YutaMatsumura1016 <yuta.train1016@gmail.com>
2021-08-19 13:04:31 +0900

    2回目のコミット

--- app/src/main/java/com/example/nyukoukanri2021_1/MainActivity.java
+++ app/src/main/java/com/example/nyukoukanri2021_1/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.nyukoukanri2021_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     static String gate = "str";
+    Button buttonWaseda = findViewById(R.id.buttonWaseda);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
