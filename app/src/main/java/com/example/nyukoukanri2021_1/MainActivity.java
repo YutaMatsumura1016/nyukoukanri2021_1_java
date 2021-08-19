3d00db7646a0278e6da74ef0fc6fa1d5d0bb686a
YutaMatsumura1016 <yuta.train1016@gmail.com>
2021-08-19 13:01:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nyukoukanri2021_1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.nyukoukanri2021_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    static String gate = "str";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
