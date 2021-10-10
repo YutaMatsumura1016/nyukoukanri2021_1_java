2da29c130a155e306ccfbaf9962cc8370db7ed1c
YutaMatsumura1016 <yuta.train1016@gmail.com>
2021-10-10 21:15:31 +0900

    デザインを追加

--- app/src/main/java/com/example/nyukoukanri2021_1/MainActivity.java
+++ app/src/main/java/com/example/nyukoukanri2021_1/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.nyukoukanri2021_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +26,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //ボタンの設定
-        Button buttonWaseda = findViewById(R.id.buttonWaseda);
-        Button buttonToyama = findViewById(R.id.buttonToyama);
-        Button buttonLogin = findViewById(R.id.buttonLogin);
-        Button buttonFinishLogin = findViewById(R.id.buttonFinishLogin);
+        ImageButton buttonWaseda = findViewById(R.id.buttonWaseda);
+        ImageButton buttonToyama = findViewById(R.id.buttonToyama);
+        ImageButton buttonKougai = findViewById(R.id.buttonKougai);
+        ImageButton buttonGakkan = findViewById(R.id.buttonGakkan);
+        ImageButton buttonLogin = findViewById(R.id.buttonLogin);
+        ImageButton buttonFinishLogin = findViewById(R.id.buttonFinishLogin);
         WebView loginWebView = findViewById(R.id.loginWebView);
         buttonFinishLogin.setVisibility(View.INVISIBLE);
+        loginWebView.setVisibility(View.INVISIBLE);
+
+        loginWebView.getSettings().setJavaScriptEnabled(true);
+        loginWebView.getSettings().setDomStorageEnabled(true);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
 
         //ボタン押された
@@ -46,9 +58,24 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("gate", gate);
             startActivity(intent);
         });
+        buttonKougai.setOnClickListener(v -> {
+            gate = "kougai";
+
+            Intent intent = new Intent(getApplication(), readActivity.class);
+            intent.putExtra("gate", gate);
+            startActivity(intent);
+        });
+        buttonGakkan.setOnClickListener(v -> {
+            gate = "gakkan";
+
+            Intent intent = new Intent(getApplication(), readActivity.class);
+            intent.putExtra("gate", gate);
+            startActivity(intent);
+        });
         buttonLogin.setOnClickListener(v -> {
+            loginWebView.setVisibility(View.VISIBLE);
             loginWebView.setWebViewClient(new WebViewClient());
-            String sentURL = "https://drive.google.com/file/d/1rq38l_a8wmZsA8mKw-2yV9M248_BlKmy/view?usp=sharing";
+            String sentURL = "https://script.google.com/a/wasedasai.net/macros/s/AKfycbzaVnrwI0sa0CUYjsz-PNF9sdiyTPZtVP6CC_ZzA3f2vpFMdC4/exec";
 
             loginWebView.loadUrl(sentURL);
             buttonFinishLogin.setVisibility(View.VISIBLE);
@@ -63,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             buttonToyama.setVisibility(View.VISIBLE);
         });
     }
+
 
 
 
--- app/src/main/java/com/example/nyukoukanri2021_1/readActivity.java
+++ app/src/main/java/com/example/nyukoukanri2021_1/readActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -47,7 +48,7 @@ public class readActivity extends AppCompatActivity {
                 .setAudioAttributes(audioAttributes)
                 .setMaxStreams(1)
                 .build();
-        soundpi = soundPool.load(this, R.raw.pi_cut, 1);
+        soundpi = soundPool.load(this, R.raw.sharan, 1);
 
         //入構キャンパスの引継ぎ
         Intent intent = getIntent();
@@ -104,14 +105,18 @@ public class readActivity extends AppCompatActivity {
                     setContentView(R.layout.activity_read);
 
                     WebView webView = findViewById(R.id.webView);
-                    TextView idmText = findViewById(R.id.idmText);
+
+                    webView.getSettings().setJavaScriptEnabled(true);
+                    webView.getSettings().setDomStorageEnabled(true);
+                    getWindow().setFlags(
+                            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
                     webView.setWebViewClient(new WebViewClient());
                     String sentURL = "https://script.google.com/a/wasedasai.net/macros/s/AKfycbzdl8gVXhd2Dkqy1B6-rTGKD_ewKWpS2FimTIkZiOA2bVf_IQo/exec?idm=" + idmString + "&&gate=" + gate;
                     webView.loadUrl(sentURL);
 
                     soundPool.play(soundpi,1.0f, 1.0f, 0, 0,1);
-                    idmText.setText(idmString);
 
                 }
             });
